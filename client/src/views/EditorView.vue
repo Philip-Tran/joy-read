2c712480cf01a431301cd9219ce8800cc94ba3f9
Quyet's Code <quyetcoccoc@gmail.com>
2024-11-29 12:14:50 +0700

    complete form fe

--- client/src/views/EditorView.vue
+++ client/src/views/EditorView.vue
@@ -1,21 +1,23 @@
 <script setup>
-import { useEditor, EditorContent } from '@tiptap/vue-3'
-import StarterKit from '@tiptap/starter-kit'
+import PostEditor from "@/components/(features)/addLesson/PostEditor.vue"
 
-const editor = useEditor({
-    editorProps: {
-        attributes: {
-            class: "prose max-w-none text-grey-700 prose-sm border min-h-60 max-h-[12rem] overflow-y-auto border-blue-500 p-4"
-        },
-        transformPastedText(text) {
-            return text.toUpperCase()
-        }
-    },
-    content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
-    extensions: [StarterKit],
+import { Input } from '@/components/ui/input';
+import { useForm } from 'vee-validate';
+import { toTypedSchema } from '@vee-validate/zod';
+import * as z from "zod"
+
+
+const { values, defineField, errors } = useForm({
+    validationSchema: toTypedSchema(
+        z.object({
+            title: z.string().min(3, "Your title is too short"),
+            content: z.string().min(25, "Too short is not worth reading").default("<h1>This is a heading</h1>This is content default")
+        })
+    )
 })
 
-
+const [title, titleAttrs] = defineField("title")
+const [content, contentAttrs] = defineField("content")
 
 </script>
 
@@ -24,101 +26,11 @@ const editor = useEditor({
         <h1>eidtor</h1>
         <div class="min-w-[600px] max-w-[600px]">
             <div>
-                <div v-if="editor" class="w-full min-h-12 p-2 rounded bg-slate-50 flex flex-wrap gap-3">
-                    <button @click="editor.chain().focus().toggleBold().run()"
-                        :disabled="!editor.can().chain().focus().toggleBold().run()"
-                        :class="{ 'is-active': editor.isActive('bold') }">
-                        Bold
-                    </button>
-                    <button @click="editor.chain().focus().toggleItalic().run()"
-                        :disabled="!editor.can().chain().focus().toggleItalic().run()"
-                        :class="{ 'is-active': editor.isActive('italic') }">
-                        Italic
-                    </button>
-                    <button @click="editor.chain().focus().toggleStrike().run()"
-                        :disabled="!editor.can().chain().focus().toggleStrike().run()"
-                        :class="{ 'is-active': editor.isActive('strike') }">
-                        Strike
-                    </button>
-                    <button @click="editor.chain().focus().toggleCode().run()"
-                        :disabled="!editor.can().chain().focus().toggleCode().run()"
-                        :class="{ 'is-active': editor.isActive('code') }">
-                        Code
-                    </button>
-                    <button @click="editor.chain().focus().unsetAllMarks().run()">
-                        Clear marks
-                    </button>
-                    <button @click="editor.chain().focus().clearNodes().run()">
-                        Clear nodes
-                    </button>
-                    <button @click="editor.chain().focus().setParagraph().run()"
-                        :class="{ 'is-active': editor.isActive('paragraph') }">
-                        Paragraph
-                    </button>
-                    <button @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
-                        :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }">
-                        H1
-                    </button>
-                    <button @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
-                        :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }">
-                        H2
-                    </button>
-                    <button @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
-                        :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }">
-                        H3
-                    </button>
-                    <button @click="editor.chain().focus().toggleHeading({ level: 4 }).run()"
-                        :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }">
-                        H4
-                    </button>
-                    <button @click="editor.chain().focus().toggleHeading({ level: 5 }).run()"
-                        :class="{ 'is-active': editor.isActive('heading', { level: 5 }) }">
-                        H5
-                    </button>
-                    <button @click="editor.chain().focus().toggleHeading({ level: 6 }).run()"
-                        :class="{ 'is-active': editor.isActive('heading', { level: 6 }) }">
-                        H6
-                    </button>
-                    <button @click="editor.chain().focus().toggleBulletList().run()"
-                        :class="{ 'is-active': editor.isActive('bulletList') }">
-                        Bullet list
-                    </button>
-                    <button @click="editor.chain().focus().toggleOrderedList().run()"
-                        :class="{ 'is-active': editor.isActive('orderedList') }">
-                        Ordered list
-                    </button>
-                    <button @click="editor.chain().focus().toggleCodeBlock().run()"
-                        :class="{ 'is-active': editor.isActive('codeBlock') }">
-                        Code block
-                    </button>
-                    <button @click="editor.chain().focus().toggleBlockquote().run()"
-                        :class="{ 'is-active': editor.isActive('blockquote') }">
-                        Blockquote
-                    </button>
-                    <button @click="editor.chain().focus().setHorizontalRule().run()">
-                        Horizontal rule
-                    </button>
-                    <button @click="editor.chain().focus().setHardBreak().run()">
-                        Hard break
-                    </button>
-                    <button @click="editor.chain().focus().undo().run()"
-                        :disabled="!editor.can().chain().focus().undo().run()">
-                        Undo
-                    </button>
-                    <button @click="editor.chain().focus().redo().run()"
-                        :disabled="!editor.can().chain().focus().redo().run()">
-                        Redo
-                    </button>
-                    <button @click="editor.chain().focus().setColor('#958DF1').run()"
-                        :class="{ 'is-active': editor.isActive('textStyle', { color: '#958DF1' }) }">
-                        Purple
-                    </button>
-                </div>
+                <Input v-model="title" class="mb-4" />
             </div>
-            <div>
-                <editor v-model="content" />
-                <editor-content class="font-serif text-yellow-300" :editor="editor" />
-            </div>
+            <p>{{ values }}</p>
+            <p>{{ errors }}</p>
+            <PostEditor v-model="content" />
 
         </div>
     </section>
